2412b98f61801f2ea3b6d2e4b01dfd30f2e2ff22
Leonard Lin <gwokae@gmail.com>
2020-02-29 23:06:38 +0800

    * basic Calendar style * add formatDate utils func * add equalsDate utils func

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,17 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import classnames from 'classnames';
 
-import { getCalendar } from './utils/date';
+import {
+  getCalendar,
+  equalsDate,
+  formatDate,
+  getDate,
+  DAY,
+  CALENDAR_DEFAULT_OPTIONS,
+} from './utils/date';
+const CELL_SIZE = '36px';
+
 const Container = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  width: 260px;
+  border: 1px solid #eee;
+  padding: 0.5em;
+  justify-content: flex-start;
+  align-items: center;
 `;
-const Calendar = () => {
-  // const { date = new Date() } = props;
-  return <Container>Hello{getCalendar(2020, 1)}</Container>;
+
+const FlexItem = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+  height: 1.5em;
+  margin: 0.3em 0;
+  flex: 0 1 auto;
+
+  &.selected {
+    background: #db3d44;
+    color: white;
+    font-weight: bold;
+  }
+  &.invalid {
+    color: #eee;
+  }
+`;
+const Navigation = styled(FlexItem)`
+  flex-basis: calc(10% - 0.5em);
+  text-align: center;
+`;
+const MainButton = styled(FlexItem)`
+  flex-basis: 80%;
+  text-align: center;
+`;
+const SevenCellsRowItem = styled(FlexItem)`
+  flex-basis: ${CELL_SIZE};
+  height: ${CELL_SIZE};
+  line-height: ${CELL_SIZE};
+`;
+
+const DateItem = styled(SevenCellsRowItem)`
+  border-radius: 100%;
+  cursor: pointer;
+`;
+
+const DayOfWeekItem = styled(SevenCellsRowItem)`
+  font-weight: bold;
+`;
+
+const FourCellsRowItem = styled(FlexItem)`
+  flex-basis: ${100 / 4}%;
+`;
+
+const Calendar = (props) => {
+  const { date = new Date(), options = CALENDAR_DEFAULT_OPTIONS } = props;
+  const [mode, setMode] = useState(date.getFullYear());
+  const [currYear, setCurrYear] = useState(date.getFullYear());
+  const [currMonth, setCurrMonth] = useState(date.getMonth());
+
+  let dayOfWeek = [];
+  for (let i = 0; i < 7; i++) {
+    dayOfWeek.push(DAY[(options.startDay + i) % DAY.length]);
+  }
+  return (
+    <Container>
+      <Navigation>&lt;</Navigation>
+      <MainButton>
+        {formatDate(getDate(currYear, currMonth), { format: 'MMM YYYY' })}
+      </MainButton>
+      <Navigation>&gt;</Navigation>
+      {dayOfWeek.map((dow) => (
+        <DayOfWeekItem key={dow.full}>{dow.sShort}</DayOfWeekItem>
+      ))}
+      {getCalendar(currYear, currMonth, options).map((d) => (
+        <DateItem
+          key={d.toISOString()}
+          className={classnames({
+            selected: equalsDate(d, date),
+            invalid: d.getMonth() !== currMonth,
+          })}
+        >
+          {d.getDate()}
+        </DateItem>
+      ))}
+    </Container>
+  );
 };
 
 Calendar.propTypes = {
   date: PropTypes.instanceOf(Date),
+  options: PropTypes.object,
 };
 export default Calendar;
--- src/utils/date.js
+++ src/utils/date.js
@@ -4,8 +4,34 @@ export const getDate = (year = 1900, month = 0, day = 1) =>
 export const addDays = (date, amount) =>
   new Date(date.getTime() + 86400000 * amount);
 
-export const GET_CALENDAR_DEFAULT_OPTIONS = {
+export const MONTH = [
+  { full: 'January', short: 'Jan' },
+  { full: 'February', short: 'Feb' },
+  { full: 'March', short: 'Mar' },
+  { full: 'April', short: 'Apr' },
+  { full: 'May', short: 'May' },
+  { full: 'June', short: 'Jun' },
+  { full: 'July', short: 'Jul' },
+  { full: 'August', short: 'Aug' },
+  { full: 'September', short: 'Sep' },
+  { full: 'October', short: 'Oct' },
+  { full: 'November', short: 'Nov' },
+  { full: 'December', short: 'Dec' },
+];
+
+export const DAY = [
+  { full: 'Sunday', short: 'Sun', sShort: 'Su' },
+  { full: 'Monday', short: 'Mon', sShort: 'Mo' },
+  { full: 'Tuesday', short: 'Tue', sShort: 'Tu' },
+  { full: 'Wednesday', short: 'Wed', sShort: 'We' },
+  { full: 'Thursday', short: 'Thu', sShort: 'Th' },
+  { full: 'Friday', short: 'Fri', sShort: 'Fr' },
+  { full: 'Saturday', short: 'Sat', sShort: 'Sa' },
+];
+
+export const CALENDAR_DEFAULT_OPTIONS = {
   startDay: 0,
+  format: 'MMM D, YYYY',
 };
 const isCalendarEnd = (base, current, options) => {
   const startDay = options.startDay === 0 ? 7 : options.startDay;
@@ -23,9 +49,10 @@ const isCalendarEnd = (base, current, options) => {
 export const getCalendar = (
   year,
   month,
-  options = GET_CALENDAR_DEFAULT_OPTIONS,
+  options = CALENDAR_DEFAULT_OPTIONS,
 ) => {
   const base = getDate(year, month);
+  base.setDate(1);
   let current = addDays(base, (base.getDay() - options.startDay) * -1);
   const result = [current];
 
@@ -34,5 +61,50 @@ export const getCalendar = (
     result.push(current);
   }
 
-  console.log(result.map((r) => r.toString()));
+  return result;
 };
+
+// workaround padStart is not avaliable on IE
+const padZeroStart = (num) => (num < 10 ? '0' : '' + num);
+
+export const formatDate = (d, options = CALENDAR_DEFAULT_OPTIONS) => {
+  const { format } = options;
+  let result = format;
+  // handle year
+  if (result.match(/YYYY/)) {
+    result = result.replace(/YYYY/, d.getFullYear());
+  } else if (result.match(/YY/)) {
+    result = result.replace(/YY/, String(d.getFullYear()).slice(2));
+  }
+
+  // handle month
+  if (result.match(/MMMM/)) {
+    result = result.replace(/MMMM/, MONTH[d.getMonth()].full);
+  } else if (result.match(/MMM/)) {
+    result = result.replace(/MMM/, MONTH[d.getMonth()].short);
+  } else if (result.match(/MM/)) {
+    result = result.replace(/MM/, padZeroStart(d.getMonth() + 1));
+  } else if (result.match(/M/)) {
+    result = result.replace(/M/, d.getMonth() + 1);
+  }
+
+  // handle date
+  if (result.match(/DD/)) {
+    result = result.replace(/DD/, padZeroStart(d.getDate()));
+  } else if (result.match(/D/)) {
+    result = result.replace(/D/, d.getDate());
+  }
+
+  // handle day of week
+  if (result.match(/dddd/)) {
+    result = result.replace(/dddd/, DAY[d.getDay()].full);
+  } else if (result.match(/ddd/)) {
+    result = result.replace(/ddd/, DAY[d.getDay()].short);
+  } else if (result.match(/dd/)) {
+    result = result.replace(/dd/, DAY[d.getDay()].sShort);
+  }
+  return result;
+};
+const equalsDateOptions = { format: 'DD MMM YYYY' };
+export const equalsDate = (d1, d2) =>
+  formatDate(d1, equalsDateOptions) === formatDate(d2, equalsDateOptions);
